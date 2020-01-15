974774b1a043b621198b4d18cd0feb43afb0eb0d
unknown <winnie.joe123@gmail.com>
2020-01-15 09:02:27 -0700

    change tester

--- Tester.java
+++ Tester.java
@@ -1,8 +1,8 @@
 public class Tester{
 	public static void main(String[] args){
-		Point point1 = new Point(0,0);
-		Point point2 = new Point(1,0);
-		Point point3 = new Point(0,1);
+		Point point1 = new Point(-5,-6);
+		Point point2 = new Point(2,3);
+		Point point3 = new Point(4,7);
 		Triangle triangle = new Triangle(point1, point2, point3);
 		System.out.println(triangle.area());
 
