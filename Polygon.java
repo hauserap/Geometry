71eac6ad2a9641421fe6b8afd699cf637712e2ac
Alex Pope Hauser <hauserap@s.dcsdk12.org>
2020-01-15 09:19:12 -0700

    build constructor

--- Polygon.java
+++ Polygon.java
@@ -1,4 +1,11 @@
 public class Polygon{
 
+    private ArrayList<Point> points = new ArrayList<Point>();
+    private int numberOfSides;
+
+    public Polygon(ArrayList<Point> points){
+        this . points = points;
+        this . numberOfSides = this . points.size();
+    }
 	
 }
