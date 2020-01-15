76e1b437acfe4319de439a437d2506e8577f106e
unknown <winnie.joe123@gmail.com>
2020-01-15 09:20:46 -0700

    perimeter method

--- Polygon.java
+++ Polygon.java
@@ -1,4 +1,13 @@
 public class Polygon{
 
-	
+
+
+	public int perimeter(){
+		int perimeter = 0;
+		for (int i = 0; i<this.numberOfSides - 1; i++){
+			perimeter += Math.abs(vertexes.get(i).distance(vertexes.get(i + 1)));
+		}
+		perimeter += Math.abs(vertexes.get(numberOfSides - 1).distance(vertexes.get(i)));
+		return perimeter;
+	}
 }
