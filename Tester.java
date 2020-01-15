72d78d8a8519410fcd4b32ef3b29d2daaa6748dc
unknown <winnie.joe123@gmail.com>
2020-01-15 09:25:20 -0700

    test polygon

--- Tester.java
+++ Tester.java
@@ -1,10 +1,22 @@
 public class Tester{
 	public static void main(String[] args){
-		Point point1 = new Point(-5,-6);
-		Point point2 = new Point(2,3);
-		Point point3 = new Point(4,7);
-		Triangle triangle = new Triangle(point1, point2, point3);
-		System.out.println(triangle.area());
+//		Point point1 = new Point(-5,-6);
+//		Point point2 = new Point(2,3);
+//		Point point3 = new Point(4,7);
+//		Triangle triangle = new Triangle(point1, point2, point3);
+//		System.out.println(triangle.area());
 
+		Point point1 = new Point(0,0);
+		Point point2 = new Point(2,0);
+		Point point3 = new Point(0,2);
+		Point point4 = new Point(2,2);
+		ArrayList<Point> points = new ArrayList<Point>()
+		points.add(point1);
+		points.add(point2);
+		points.add(point3);
+		points.add(point4);
+
+		Polygon square = new Polygon();
+		System.out.println(triangle.area());
 	}
 }
